547ccccd1f4cd21024f4f4fb29a8cfdf8f318021
Georgi Yolovski <bluef0rever@abv.bg>
2013-08-16 23:54:55 +0300

    Added models and data layer

---
+++ CodeJewels.DataLayer/CodeJewelsContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeJewels.Models;
+
+namespace CodeJewels.DataLayer
+{
+    public class CodeJewelsContext : DbContext
+    {
+        public CodeJewelsContext() : base("CodeJewelsDb")
+        {
+            
+        }
+
+        public DbSet<Jewel> Jewels { get; set; } 
+    }
+}
